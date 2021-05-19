daa022ffaaa22146129af8c4de14f64d3312a2e0
shreya raj <shreyaraj25032002@gmail.com>
2021-05-19 13:36:12 +0530

    second commit

--- resume1.js
+++ resume1.js
@@ -4,6 +4,3 @@ document.querySelector(".add1").addEventListener("click",function(){
 function add1(){
    document.querySelector(".sociallinks").classList.remove("sociallinks");
  }
-document.querySelector(".add2").addEventListener("click",function(){
-  add2();
-})
