4a5b7aac9859e3deead9d31d439f60a11845965a
Satyam <satyam3225agarwal@gmail.com>
2021-05-19 15:02:37 +0530

    added resume

---
+++ form/resume1.js
@@ -0,0 +1,6 @@
+document.querySelector(".add1").addEventListener("click",function(){
+  add1();
+});
+function add1(){
+   document.querySelector(".sociallinks").classList.remove("sociallinks");
+ }
