39fae892c78dd698b28c68a3bc9fe0ac0f0576ac
Satyam <satyam3225agarwal@gmail.com>
2021-05-20 13:57:14 +0530

    fourth

--- Hello/project1/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from project1.models import Contact,Signup
-
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(Signup)
--- form/resume1.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".add1").addEventListener("click",function(){
-  add1();
-});
-function add1(){
-   document.querySelector(".sociallinks").classList.remove("sociallinks");
- }
